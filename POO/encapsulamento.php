a3586da228c3424a0d73918fab2573d5ab3ca8a1
Moises Sousa <moises.sousa583@gmail.com>
2023-04-15 13:18:59 -0300

    Encapsulamento no contexto de herança

--- POO/encapsulamento.php
+++ POO/encapsulamento.php
@@ -6,26 +6,40 @@ class Pai
     protected $sobrenome = "Silva";
     public $humor = "Feliz";
 
-    public function __get($atributo)
+    // public function __get($atributo)
+    // {
+    //     return $this->$atributo;
+    // }
+
+    // public function __set($atributo, $valor)
+    // {
+    //     $this->$atributo = $valor;
+    // }
+}
+
+class Filho extends Pai
+{
+    public function getAtributo($atributo)
     {
         return $this->$atributo;
     }
-
-    public function __set($atributo, $valor)
-    {
-        $this->$atributo = $valor;
-    }
 }
 
-$pai = new Pai();
-echo $pai->__get("nome");
-echo "<br>";
-$pai->__set("nome", "Moises");
-echo "<br>";
-echo $pai->__get("nome");
-echo "<br>";
-echo $pai->__get('sobrenome');
-echo "<br>";
-$pai->__set('sobrenome', "Sousa");
-echo "<br>";
-echo $pai->__get('sobrenome');
+// $pai = new Pai();
+// echo $pai->__get("nome");
+// echo "<br>";
+// $pai->__set("nome", "Moises");
+// echo "<br>";
+// echo $pai->__get("nome");
+// echo "<br>";
+// echo $pai->__get('sobrenome');
+// echo "<br>";
+// $pai->__set('sobrenome', "Sousa");
+// echo "<br>";
+// echo $pai->__get('sobrenome');
+
+$filho = new Filho();
+print_r($filho);
+echo $filho->getAtributo("humor");
+echo $filho->getAtributo("sobrenome");
+echo $filho->getAtributo("nome");
